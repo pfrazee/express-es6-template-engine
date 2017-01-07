8f94d8e2e53a9102b1e35b96d0b12e1c0e7a7478
Paul Frazee <pfrazee@gmail.com>
2017-01-06 18:03:04 -0600

    clean up and fix the rendered locals

--- es6-renderer.js
+++ es6-renderer.js
@@ -1,45 +1,46 @@
-const fs = require('fs'); // this engine requires the fs module
-module.exports = (function(options) { // define the template engine
+const fs = require('fs') // this engine requires the fs module
+module.exports = (function (options) { // define the template engine
   /* jshint ignore:start */
-  const interpolate = (content, keyList, valList) => new Function(
-      ...keyList,
-      'return `' + content + '`;'
-    )(...valList),
-    /* jshint ignore:end */
-    readPartial = (filePath) => new Promise(
-      (resolve, reject) => fs.readFile(
-        filePath,
-        'utf-8',
-        (err, content) => err ? reject(new Error(err))  : resolve(content)
-      )
-    );
-  this.render = (filePath, dict, callback) => {
+  const interpolate = (content, keyList, valList) => (new Function(...keyList, 'return `' + content + '`;')(...valList))
+  /* jshint ignore:end */
+  function readPartial (filePath) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, 'utf-8', (err, content) => {
+        if (err) reject(err)
+        else resolve(content)
+      })
+    })
+  }
+
+  this.render = (filePath, locals, callback) => {
     const compile = (err, content) => {
-      var locals = dict.locals || {},
-        localsList = Object.keys(locals),
-        partialsList = Object.keys(dict.partials || {}),
-        valList = localsList.map(i => locals[i]),
-        keyList = localsList.concat(partialsList);
-      if(err) {
-        return callback(new Error(err));
+      var localKeys = Object.keys(locals)
+      var localValues = localKeys.map(k => locals[k])
+      var partialKeys = Object.keys(locals.partials || {})
+      // var keyList = localKeys.concat(partialKeys)
+      if (err) {
+        return callback(new Error(err))
       }
-      if(partialsList.length) {
-        return Promise.all(partialsList.map(i => readPartial(dict.partials[i])))
-          .then(values => {
-            const valTempList = valList.concat(values);
-            valList = valList.concat(
-              values.map(i => interpolate(i, keyList, valTempList))
-            );
-            return callback(null, interpolate(content, keyList, valList));
+      if (partialKeys.length && !locals.partials.__rendered__) {
+        // read all partials
+        console.log('all partials:',locals.partials)
+        console.log('partialkeys:', partialKeys)
+        return Promise.all(partialKeys.map(k => readPartial(locals.partials[k])))
+          .then(partialValues => {
+            // render the partials
+            partialKeys.forEach((k, index) => {
+              locals.partials[k] = interpolate(partialValues[index], localKeys, localValues)
+            })
+            // mark as rendered
+            locals.partials.__rendered__ = true
+            // render the template
+            return callback(null, interpolate(content, localKeys, localValues))
           })
-          .catch(err => callback(err));
+          .catch(err => callback(err))
       }
-      return callback(null, interpolate(content, keyList, valList));
-    };
-    if (dict.template) {
-      return compile(null, filePath);
+      return callback(null, interpolate(content, localKeys, localValues))
     }
-    fs.readFile(filePath, 'utf-8', compile);
-  };
-  return this.render;
-})();
+    fs.readFile(filePath, 'utf-8', compile)
+  }
+  return this.render
+})()
